76ffa2d07f901b77491e6c6b2223727c7493146c
m-ruhtra <arthur.mig.rodrigues@gmail.com>
2024-05-09 16:52:46 -0300

    feat: loginPage informations implemented

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -4,12 +4,19 @@ import styles from "./style.module.scss";
 export const LoginPage = () => {
     return(
         <div className="container loginPage">
-            <div className={styles.login_div}>
-                <p>OLÁMUNDO</p>
-            </div>
-            
-            <img id="backgoundLogin" src={backgroundLogin} alt="Background image" />
+            <div className={styles.container_content}>
+                <div className={styles.login_div}>
+                    <h1 className="title loginPage">HQMarvel</h1>
 
+                    <p className="text">A ação mais perto de você!</p>
+
+                    <form action="">
+                        <input type="text" />
+                    </form>
+                </div>
+                
+                <img id="backgoundLogin" src={backgroundLogin} alt="Background image" />
+            </div>
         </div>
     )
 };
